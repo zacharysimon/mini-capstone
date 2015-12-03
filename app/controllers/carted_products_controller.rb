d9527149d6ef25ff7e26eadc92b89b6e39d4d488
zacharysimon <zps1009@gmail.com>
2015-12-02 19:45:49 -0600

    restyled product show page and redirected user from cart if cart is empty

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,21 +1,45 @@
 class CartedProductsController < ApplicationController
 
   def create
-    @carted_products = CartedProduct.create(
-      user_id: current_user.id,
-      quantity: params[:input_quantity],
-      product_id: params[:input_product_id],
-      status: "carted"
-    )
-    redirect_to '/carted_products'
+    if current_user
+      @carted_products = CartedProduct.create(
+        user_id: current_user.id,
+        quantity: params[:input_quantity],
+        product_id: params[:input_product_id],
+        status: "carted"
+      )
+      redirect_to '/carted_products'
+    else
+      redirect_to '/products'
+      flash[:warning] = "Sign in to add this to your cart!"
+    end
   end
 
-   def index
-    if current_user
+  def index
+
+   if current_user
       @cart_items = current_user.carted_products.where("status LIKE ?","carted")
+
+      if @cart_items.length == 0
+        redirect_to '/products'
+      end 
     else
-      redirect_to '/users/sign_up'
-    end
+        redirect_to '/products'
+    end 
+
+  end
+
+  def update
+   @cart_item = CartedProduct.find_by(id: params[:id])
+
+   @cart_item.update(
+    status: "removed"
+    )
+
+
+  flash[:success] = "Product was successfully removed from cart!"
+  redirect_to "/carted_products"
+
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
 
   post "/carted_products" => 'carted_products#create'
   get '/carted_products' => 'carted_products#index'
+  patch '/carted_products/:id' => 'carted_products#update'
 
 
 
